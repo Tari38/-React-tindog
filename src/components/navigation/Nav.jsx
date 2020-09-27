afe0c5ef6b191d660b2ceac3104ccb5715b9b283
Samantha <51409073+Tari38@users.noreply.github.com>
2020-09-27 09:46:41 +0100

    updated href for tindog

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -7,22 +7,36 @@ import "./Nav.css";
 function NavBar() {
 
   return (
-    <>
+<>
 
-  <Navbar className="navbar justify-content-between" dark expand="md">
-    <div className="navbar-brand">
-      <h1>tindog</h1>
-    </div>
+    <section className="colored-section" id="title">
+      <div className="container-fluid">
+      
+      <Navbar className="navbar justify-content-between" dark expand="md">
+          <div>
+            <a className="navbar-brand" href="/">tindog</a>
+          </div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarTogglerDemo02">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <Link className="navlink" to={"#cta"}>Contact</Link>
+                <Link className="navlink" to={"#pricing"}>Pricing</Link>
+                <Link className="navlink" to={"#footer"}>Download</Link>
+              </ul>
+            </div>
+      </Navbar>
 
-    <div className="navlink">
-      <Link className="navlink" to="./components/sections/pricing">Pricing</Link>
-      <Link className="navlink" to="./components/sections/contact">Contact</Link>
-      <Link className="navlink" to="./components/sections/download">Download</Link>
-    </div>
-    
-  </Navbar>
+      </div>
+    </section>
     </>
+   
     )
   }
 
   export default NavBar;
+
+
+
